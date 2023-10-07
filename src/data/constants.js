2d7a0ceb2c271cad91e571a4cc6e6123f4c8f2b6
vintha92 <141485349+lilacollier7@users.noreply.github.com>
2023-10-06 23:44:10 -0700

    god

---
+++ src/data/constants.js
@@ -0,0 +1,109 @@
+export const Bio = {
+  name: "Ivan on Tech",
+  roles: [
+    "Gold Expert",
+    "Real Estate Advisor",
+    "Precious Stones MASTER",
+    "Stock Master",
+    "Crypto MASTER"
+  ],
+  description:
+    "As a 30 year old real estate agent and investor who started working in real estate shortly after turning 18, with over $120,000,000 in residential real estate sales since 2008, I've created this channel to share my successes, failures, and experiences in the real estate industry and to support others who are thinking of getting into the business. Oh, and I'm also obsessed with cars. A lot. So expect to see some cars uploaded to my channel. A lot. And Subscribe. Because all the cool kids are doing it. Feel free to follow me on Snapchat / Instagram: GPStephan",
+  github: "https://www.youtube.com/@IvanOnTech",
+  resume:
+    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
+  twitter: "https://twitter.com/RishavChanda",
+  insta: "https://www.instagram.com/rishav_chanda/",
+  facebook: "https://www.facebook.com/rishav.chanda.165/",
+};
+
+export const skills = [
+  
+];
+
+export const experiences = [
+
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://firebasestorage.googleapis.com/v0/b/fileupload-74efb.appspot.com/o/check-mark-2.png?alt=media&token=503b3696-1b13-4711-bb3d-bb635708cd83&_gl=1*1s41gsx*_ga*NzE2MTQ4ODUuMTY5NjU4NTkxMQ..*_ga_CW55HF8NVT*MTY5NjU4NTkxMS4xLjEuMTY5NjU4NjQ5Mi4zNy4wLjA.",
+    school: "Stock Trading",
+    date: "",
+    grade: "100%",
+    desc: "Silver Stocks Rick Rule",
+    degree: "Gold stocks, Gold mining, Investing, precious metals, Silver Mines, Silver Mining",
+  },
+  {
+    id: 1,
+    img: "https://firebasestorage.googleapis.com/v0/b/fileupload-74efb.appspot.com/o/checkmark.png?alt=media&token=edeaba8b-b5f3-4af2-b9cf-31960e582c46&_gl=1*1lebb22*_ga*NzE2MTQ4ODUuMTY5NjU4NTkxMQ..*_ga_CW55HF8NVT*MTY5NjU4NTkxMS4xLjEuMTY5NjU4NjUyNy4yLjAuMA..",
+    school: "Inflation, hyperinflation - Silver stocks to buyock Trading",
+    date: "",
+    grade: "100%",
+    desc: "Silver stocks US",
+    degree: "Gold Silver stocks",
+  },
+  {
+    id: 2,
+    img: "https://firebasestorage.googleapis.com/v0/b/fileupload-74efb.appspot.com/o/check-mark.png?alt=media&token=ea60f442-b3f4-4b44-b71b-30ea3db7c3a5&_gl=1*1dibh35*_ga*NzE2MTQ4ODUuMTY5NjU4NTkxMQ..*_ga_CW55HF8NVT*MTY5NjU4NTkxMS4xLjEuMTY5NjU4NjU2OS4yMC4wLjA.",
+    school: "Investing gold coins - Uranium, uranium stocks, uranium stocks to buy",
+    date: "",
+    grade: "100%",
+    desc: "Gold Rush",
+    degree: "Gold investing for beginners & Precious metals investing",
+  },
+];
+
+export const projects = [
+  
+  {
+    id: 5,
+    title: "Americans Will Run Out Of Money By January 1st",
+    date: "oct 06, 2023",
+    description:
+      "Research from the San Fransisco FED suggested that “Americans have burned through more than 90% of the “excess savings” they amassed in 2020 and 2021. What little remains, the economists estimate, is likely to be gone by the end of September. Other Federal Reserve members have also previously predicted that “excess savings would run out earlier this year - which proved to be inaccurate",
+    image:
+      "https://raw.githubusercontent.com/lilacollier7/portfolio3/main/src/images/mq1.webp",
+    tags: ["Money", "Savings", "Investment", "Spending"],
+    category: "spending",
+    github: "https://www.youtube.com/@IvanOnTech",
+    webapp: "https://www.youtube.com/watch?v=jtoBu2kwSj0",
+  },
+  {
+    id: 6,
+    title: "BREAKING: Government Shutdown Imminent, Rates Spike, Stocks Collapse",
+    date: "Sep 26, 2023",
+    description:
+      "THE GOVERNMENT SHUTDOWN: Even though Congress previously agreed to raise the total debt ceiling until 2025, how they actually allocate that money is another matter entirely. In typical situations, these budgets are agreed upon by September 30th so that all spending is predetermined throughout the following fiscal year - and if that doesn't happen - we have a government shutdown. Under the anti-deficiency act of 1884, federal agencies cannot spend or obligate any money without an appropriation from Congress. Therefore, if there is no bipartisan agreement by September 30th, federal agencies must cease all non-essential functions. This includes the collection, processing, and dissemination of government data, including employment and inflation figures, of which the Federal Reserve needs to make it’s next rate hike decision on November 1st.",
+    image:
+      "https://raw.githubusercontent.com/lilacollier7/portfolio3/main/src/images/mq2.webp",
+    tags: ["Government", "Stock", "Rates", "Bulls"],
+    category: "web app",
+    github: "https://www.youtube.com/@IvanOnTech",
+    webapp: "https://www.youtube.com/watch?v=keCuFwLDFUA",
+  },
+  {
+    id: 7,
+    title: "How To Buy Your First Rental Property (Step by Step)",
+    date: "Nov 19, 2018",
+    description:
+      "How to invest in real estate: Here’s exactly how you can buy your first rental property, step by step, and the process involved. Enjoy! Add me on Snapchat/Instagram: GPStephan",
+    image:
+      "https://raw.githubusercontent.com/lilacollier7/portfolio3/main/src/images/mqp.webp",
+    tags: ["property", "Lending", "Rental", "Housing"],
+    category: "Real Estate",
+    github: "https://www.youtube.com/@IvanOnTech",
+    webapp: "https://www.youtube.com/watch?v=bJx7_1rWC6U",
+  },
+  
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
